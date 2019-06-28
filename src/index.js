4ee09c91c54495c51b91d02639ad713ada23f15c
Dom Mentee <chuku0929@DomsMacBP.local>
2019-06-28 06:28:22 -0400

    first commit with server setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+//schema describes functionally avaliable to clients
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+//resolvers key used to fetch data
+//query is what happens when the query is called
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//start server
+server.listen().then(({url}) => console.log(`server sterted ${url}`)) 
